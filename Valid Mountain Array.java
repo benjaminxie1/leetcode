88067711ae7346759f9f5c3f6f763635d40ac6a1
benjaminxie1 <87211469+benjaminxie1@users.noreply.github.com>
2022-08-12 08:43:58 -0700

    Update Valid Mountain Array.java

--- Valid Mountain Array.java
+++ Valid Mountain Array.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int climb = 0;
+            while(climb < arr.length-1 && arr[climb] < arr[climb+1]){
+                climb++;
+            }
+            if(climb == 0 || climb == arr.length-1){
+                return false;
+            }
+            while(climb < arr.length-1 && arr[climb] > arr[climb+1]){
+                climb++;
+            }
+            if(climb == arr.length-1){
+                return true;
+            }
+        else return false;
+    }
+}
